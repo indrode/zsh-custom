27cd1883ccb15cef57e792dd14b786ecc5312b4f
Indro De <indro.de@gmail.com>
2012-09-26 21:06:38 +0200

    Add logging

--- scripts/zsh-watch_outgoing.sh
+++ scripts/zsh-watch_outgoing.sh
@@ -20,10 +20,11 @@ check() {
       done
 
       echo "[`date -v1m -v+1y`] Found changes in outgoing folder ($chsum2)" >> /var/log/zshcustom.log
-      scp "$OUTGOING"/*.* $TARGET_HOST:~/Incoming | tee /var/log/zshcustom.log
+      scp "$OUTGOING"/*.* $TARGET_HOST:~/Incoming 2> /var/log/zshcustom.log
       echo "[`date -v1m -v+1y`] Moved to $TARGET_HOST:~/Incoming" >> /var/log/zshcustom.log
       cd "$OUTGOING" && rm -f *.* | tee /var/log/zshcustom.log
       echo "[`date -v1m -v+1y`] Clean up succesfull" >> /var/log/zshcustom.log
+      cd /Applications && ./terminal-notifier.app/Contents/MacOS/terminal-notifier -message "File moved to $TARGET_HOST." >> /var/log/zshcustom.log
     done
 }
 
