bf8da216ca49342dd3fef9f6782e874225dea6f1
Indro De <indro.de@gmail.com>
2014-02-21 15:28:04 +0100

    Switch iTerm2 tab color when ssh'ing

---
+++ scripts/iterm2.zsh
@@ -0,0 +1,29 @@
+# iTerm2 window/tab color commands
+# found here: https://gist.github.com/wadey/1140259
+
+#  http://code.google.com/p/iterm2/wiki/ProprietaryEscapeCodes
+tab-color() {
+    echo -ne "\033]6;1;bg;red;brightness;$1\a"
+    echo -ne "\033]6;1;bg;green;brightness;$2\a"
+    echo -ne "\033]6;1;bg;blue;brightness;$3\a"
+}
+tab-reset() {
+    echo -ne "\033]6;1;bg;*;default\a"
+}
+
+# Change the color of the tab when using SSH
+# reset the color after the connection closes
+color-ssh() {
+    if [[ -n "$ITERM_SESSION_ID" ]]; then
+        trap "tab-reset" INT EXIT
+        if [[ "$*" =~ "production|ec2-.*compute-1" ]]; then
+            tab-color 255 0 0
+        else
+            tab-color 0 255 0
+        fi
+    fi
+    ssh $*
+}
+compdef _ssh color-ssh=ssh
+
+alias ssh=color-ssh
