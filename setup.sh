7f6c497f902d393ff6b50ae7c5772e6b13b13681
Indro De <indro.de@gmail.com>
2015-03-18 13:20:51 +0100

    Set up intro file from template

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 cp -n .settings/CONFIG.template .settings/config
+cp -n .settings/INTRO.template .settings/intro
 echo "Setup complete!"
 echo "Now edit the configuration file in $ZSHCUSTOM/.settings/config to your liking"
