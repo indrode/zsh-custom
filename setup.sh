b61922c5b13a28eb7015e2e289212dc13229c003
Indro De <indro.de@gmail.com>
2015-03-18 12:54:24 +0100

    Do not override settings files

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
-cp .settings/CONFIG.template .settings/CONFIG.setting
+cp -n .settings/CONFIG.template .settings/CONFIG.setting
 echo "Setup complete!"
-echo "Now edit the configuration file in $ZSHCUSTOM/.settings/CONFIG.setting"
+echo "Now edit the configuration file in $ZSHCUSTOM/.settings/CONFIG.setting to your liking"
