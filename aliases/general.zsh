56774827c84ec3388878579d56442ee26f59e1cb
Indro De <indro.de@gmail.com>
2020-06-02 17:12:48 +0200

    Keep bundle exec rspec in history

--- aliases/general.zsh
+++ aliases/general.zsh
@@ -16,12 +16,12 @@ alias G='| grep --color=auto -i'
 alias cp='cp -v'
 alias mv='mv -v'
 
-#history cleaners
-alias ls=' ls'
+#history erasers
+alias cd=' cd'
 
 #chruby
-alias r222="source /usr/local/opt/chruby/share/chruby/chruby.sh && chruby ruby-2.2.2"
-alias r230="source /usr/local/opt/chruby/share/chruby/chruby.sh && chruby ruby-2.3.0"
+# alias r222="source /usr/local/opt/chruby/share/chruby/chruby.sh && chruby ruby-2.2.2"
+# alias r230="source /usr/local/opt/chruby/share/chruby/chruby.sh && chruby ruby-2.3.0"
 
 #alias
 alias aliases="cat $ZSHCUSTOM/aliases/git.zsh $ZSHCUSTOM/aliases/general.zsh"
@@ -41,8 +41,8 @@ alias pid="ps x | grep -i $1"
 alias grep="grep --color=auto"
 
 #navigation
-alias hh='history'
-alias hs='history | grep --color=auto'
+alias hh=' history'
+alias hs=' history | grep --color=auto'
 alias tailh='history | tail -n20'
 alias ..='cd ..'
 alias ...='cd ../..'
@@ -53,15 +53,15 @@ alias .......='cd ../../../../../..'
 alias ........='cd ../../../../../../..'
 alias .........='cd ../../../../../../../..'
 alias ..........='cd ../../../../../../../../..'
-alias ls='ls -G'
-alias l='ls -lha'
-alias ll='ls -lha'
-alias cdf='cd `osascript -e "tell application \"Finder\" to if window 1 exists then if target of window 1 as string is not \":\" then get POSIX path of (target of window 1 as alias)"`'
+alias ls=' ls -G'
+alias l=' ls -lha'
+alias ll=' ls -lha'
+alias cdf=' cd `osascript -e "tell application \"Finder\" to if window 1 exists then if target of window 1 as string is not \":\" then get POSIX path of (target of window 1 as alias)"`'
 
 #tools
-alias lastfm='ruby ~/projects/code/ruby/recent_scrobbles.rb $1'
-alias init-transfer='bash $ZSHCUSTOM/scripts/zsh-watch_outgoing.sh &'
-alias start-spotify="ssh soma 'open /Applications/Spotify.app'"
+# alias lastfm='ruby ~/projects/code/ruby/recent_scrobbles.rb $1'
+# alias init-transfer='bash $ZSHCUSTOM/scripts/zsh-watch_outgoing.sh &'
+# alias start-spotify="ssh soma 'open /Applications/Spotify.app'"
 
 #zeus
 alias z='zeus'
@@ -91,7 +91,8 @@ alias stop-redis='brew services stop redis'
 alias stfu="osascript -e 'set volume output muted true'"
 alias quiet="osascript -e 'set volume 0.5'"
 alias pumpitup="osascript -e 'set volume 10'"
-alias hax="growlnotify -a 'Activity Monitor' 'System error' -m 'WTF R U DOIN'"
+# alias hax="growlnotify -a 'Activity Monitor' 'System error' -m 'WTF R U DOIN'"
+# alias hax="osascript -e 'display notification \"Notification text\" with title \"Title\"'"
 
 # MAC manipulators (see https://gist.github.com/jashkenas/5113149)
 alias random-mac='sudo ifconfig en0 ether `openssl rand -hex 6 | sed "s/\(..\)/\1:/g; s/.$//"`'
